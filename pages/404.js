f7ac8de62209ad7e2e079631ed3088a5dd215a36
konstapascal <pascalkonstantinos@gmail.com>
2021-05-19 21:28:26 +0200

    refine 404

--- pages/404.js
+++ pages/404.js
@@ -5,11 +5,11 @@ function Error404() {
 				<div className='relative flex flex-col items-center justify-center min-h-screen font-semibold text-gray-100'>
 					<div
 						id='watermark'
-						className=' text-main-light whitespace-nowrap  absolute text-[6rem] sm:text-[8rem] md:text-[10rem] font-extrabold top-[20%] lg:text-[12rem] text-center opacity-50 select-none'>
+						className=' text-main-light whitespace-nowrap  absolute text-[6rem] sm:text-[8rem] md:text-[10rem] font-extrabold top-[22.5%] lg:text-[10rem] text-center opacity-50 select-none'>
 						<p>ERROR 404</p>
 						<p>PAGE NOT FOUND!</p>
 					</div>
-					<div className=' top-1/4 sm:top-[27.5%] md:top-[30%]  lg:top-[32.5%] absolute text-center'>
+					<div className=' top-[27.5%] sm:top-[30%] md:top-[32.5%] absolute text-center'>
 						<p className=' lg:text-6xl md:text-5xl text-4xl'>
 							Error <span className='text-accent-light'>404</span> - Page
 							not found!
