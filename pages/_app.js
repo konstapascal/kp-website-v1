670956f21f89c6c7e50c71c28cfb2aaf1584ea05
konstapascal <pascalkonstantinos@gmail.com>
2021-07-08 19:55:15 +0200

    removed unused code from custom app

--- pages/_app.js
+++ pages/_app.js
@@ -49,26 +49,6 @@ function MyApp({ Component, pageProps }) {
 				</a>
 			</div>
 
-			{/* TODO: DELETE THIS LATER */}
-			{/* <div className='text-md left-0-0 fixed bottom-0 z-50 p-4 font-bold text-center text-black'>
-				<p className='sm:hidden px-1 bg-yellow-400 rounded'>XS (0px to 640px)</p>
-				<p className='sm:inline-block md:hidden hidden px-1 bg-yellow-400 rounded'>
-					SM (640px to 768px)
-				</p>
-				<p className='sm:hidden md:inline-block lg:hidden hidden px-1 bg-yellow-400 rounded'>
-					MD (768px to 1024px)
-				</p>
-				<p className='lg:inline-block xl:hidden hidden px-1 bg-yellow-400 rounded'>
-					LG (1024px to 1280px)
-				</p>
-				<p className='xl:inline-block 2xl:hidden hidden px-1 bg-yellow-400 rounded'>
-					XL (1280px to 1536px)
-				</p>
-				<span className='2xl:inline-block hidden px-1 bg-yellow-400 rounded'>
-					2XL (1536px+)
-				</span>
-			</div> */}
-			{/* TODO: DELETE THIS LATER */}
 			<Component {...pageProps} />
 		</>
 	);
