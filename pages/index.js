c5fb1836a12d84f5a1ba39beae074ea2869e66bb
konstapascal <pascalkonstantinos@gmail.com>
2021-08-06 22:29:07 +0200

    added analytics script tags, 3rd try

--- pages/index.js
+++ pages/index.js
@@ -15,11 +15,17 @@ export default function Home() {
 				<script async src='https://www.googletagmanager.com/gtag/js?id=G-Z2FSLL37JV'></script>
 				<script
 					dangerouslySetInnerHTML={{
-						__html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}
-					gtag('js', new Date()); gtag('config', 'G-Z2FSLL37JV');`
+						__html: `
+						window.dataLayer = window.dataLayer || [];
+
+						function gtag() {
+							dataLayer.push(arguments);
+						}
+						
+						gtag('js', new Date());
+						gtag('config', 'G-Z2FSLL37JV');`
 					}}
 				/>
-				<script></script>
 			</Head>
 			<HomeHero />
 
