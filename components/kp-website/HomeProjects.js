a7d0a3f1a3f93e25fe319a301274fb7fdfdd13f8
konstapascal <pascalkonstantinos@gmail.com>
2021-08-08 12:43:21 +0200

    added vignettes, increased top margin on button

--- components/kp-website/HomeProjects.js
+++ components/kp-website/HomeProjects.js
@@ -46,11 +46,11 @@ function Projects() {
 							labels={project.labels}
 						/>
 					))}
-					<a
-						className='text-sm mt-12 hover:scale-105 transform-gpu transition-transform duration-200 lg:text-lg  px-4 py-2 font-semibold tracking-wider text-gray-100  border-2 border-green-400 rounded-[0.2rem] cursor-pointer'
+					<button
+						className='text-md mt-14 lg:mt-20 hover:scale-105 transform-gpu transition-transform duration-200 lg:text-lg  px-4 py-2 font-semibold tracking-wider text-gray-100  border-2 border-green-400 rounded-[0.2rem] cursor-pointer'
 						onClick={() => setProjectsHidden(prev => !prev)}>
 						{projectsHidden ? 'Show More' : 'Show Less'}
-					</a>
+					</button>
 				</div>
 			</div>
 		</section>
