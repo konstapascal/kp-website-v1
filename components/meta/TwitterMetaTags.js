1355dc2d8654ce7607cdfe286397998a5122a8b1
konstapascal <pascalkonstantinos@gmail.com>
2021-09-12 22:12:31 +0200

    testing out twitter meta tags, second iteration

--- components/meta/TwitterMetaTags.js
+++ components/meta/TwitterMetaTags.js
@@ -1,17 +1,17 @@
 function TwitterMetaTags() {
 	return (
 		<>
-			{/* <meta name='twitter:card' content='summary_large_image' /> */}
+			<meta name='twitter:card' content='summary_large_image' />
 			<meta
 				name='twitter:description'
 				content='How to create your first npm package and publish it.'
 			/>
 			<meta name='twitter:title' content='Creating your first npm package' />
 			<meta name='twitter:site' content='@konstapascal' />
-			{/* <meta
+			<meta
 				name='twitter:image'
 				content='https://www.danvega.me/assets/static/npm_cover.bd64798.eced3da.png'
-			/> */}
+			/>
 			<meta name='twitter:creator' content='@konstapascal' />
 		</>
 	);
