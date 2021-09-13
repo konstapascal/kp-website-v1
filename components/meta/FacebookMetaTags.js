b3194c2d0a5757180e383587523a3541ee20b8c3
konstapascal <pascalkonstantinos@gmail.com>
2021-09-13 18:41:17 +0200

    change meta property to meta name

--- components/meta/FacebookMetaTags.js
+++ components/meta/FacebookMetaTags.js
@@ -1,11 +1,11 @@
 function FacebookMetaTags({ url, type, title, description }) {
 	return (
 		<>
-			<meta property='og:url' content={url} />
-			<meta property='og:type' content={type} />
-			<meta property='og:title' content={title} />
-			<meta property='og:description' content={description} />
-			<meta property='og:image' content='https://konstapascal.dev/img/kp-thumbnail.jpg' />
+			<meta name='og:url' content={url} />
+			<meta name='og:type' content={type} />
+			<meta name='og:title' content={title} />
+			<meta name='og:description' content={description} />
+			<meta name='og:image' content='https://konstapascal.dev/img/kp-thumbnail.jpg' />
 		</>
 	);
 }
