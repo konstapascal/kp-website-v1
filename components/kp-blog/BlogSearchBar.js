67390233dd02845f3b224ba4eaaa6a9880c49f59
konstapascal <pascalkonstantinos@gmail.com>
2021-09-30 07:09:05 +0200

    fixed label and input placeholder

--- components/kp-blog/BlogSearchBar.js
+++ components/kp-blog/BlogSearchBar.js
@@ -4,14 +4,14 @@ function BlogSearchBar({ search, setSearch, filterBy, setFilterBy }) {
 	return (
 		<div>
 			<label htmlFor='search' className='sr-only'>
-				Search articles by title, label or author
+				Search articles by title, excerpt, label or author
 			</label>
 			<input
 				id='search'
 				type='text'
 				name='search'
 				aria-label='Search articles'
-				placeholder='Search articles by title, label or author'
+				placeholder='Search articles by title, excerpt, label or author'
 				className='bg-main-dark w-full px-4 py-2 mt-2'
 				onChange={e => setSearch(e.target.value.trim())}
 				value={search}
